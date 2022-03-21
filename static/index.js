3a359c8febf7b3b4125fb8cd6b919ba1fa80c68e
vicky-playground <vicky.kuo.contact+1@gmail.com>
2022-03-22 05:09:03 +0800

    signin/signup

--- static/index.js
+++ static/index.js
@@ -141,3 +141,10 @@ function search() {
   }
 }
 
+function openForm() {
+  document.getElementById("login").style.display = "block";
+}
+
+function closeForm() {
+  document.getElementById("login").style.display = "none";
+}
